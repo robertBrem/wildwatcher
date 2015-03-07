0779c7eb1d5786fd69eb0c78e9aa56dd85bf3b21
bremRobert <brem_robert@hotmail.com>
2015-03-07 17:35:47 +0100

    added first test

--- src/test/java/ch/wildwatcher/control/MonitoringServiceTest.java
+++ src/test/java/ch/wildwatcher/control/MonitoringServiceTest.java
@@ -1,14 +1,25 @@
 package ch.wildwatcher.control;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
+import java.net.InetAddress;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class MonitoringServiceTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	MonitoringService service;
+
+	@Before
+	public void setUp() {
+		service = new MonitoringService();
 	}
 
+	@Test
+	public void should_return_the_converted_ip_address() {
+		InetAddress ipAddress = service.getHost("10.12.13.14");
+		byte[] expected = new byte[] { 10, 12, 13, 14 };
+		assertArrayEquals(expected, ipAddress.getAddress());
+	}
 }
